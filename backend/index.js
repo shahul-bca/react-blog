96828c45cf98a093cf5072afa0c6cfa8c7293596
shahul-bca <pshahul913@gmail.com>
2024-11-18 14:04:11 +0530

    Database connected and dotenv added succesdsfully

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
+import env from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+env.config();
+mongoose
+  .connect(process.env.mongo)
+  .then(console.log(`Mongodb connected`))
+  .catch((err) => console.log("failed database connection" + err));
 
+app.use(express.json());
 
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("server starting at port 3000");
+app.listen(PORT, () => {
+  console.log(`server running port ${PORT}`);
 });
